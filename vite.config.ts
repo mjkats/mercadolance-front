dcef9406a50162de5f8a3f6b205ecde5bc59f4c4
mjkats <michelkatsilis@gmail.com>
2025-05-29 22:38:05 -0300

    adding tailwindcss to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: ['abf8-2804-29b8-50eb-9416-e66b-ec44-9968-d1a9.ngrok-free.app']
   }
